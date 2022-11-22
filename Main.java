f48dbd48fe4b99d65a7c4cf1c4f43682b8625a75
Juan Daza <115562720+Dazaiyan@users.noreply.github.com>
2022-11-21 22:16:25 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main (String[] args) {
+        NumeroAleatorio num1 = new NumeroAleatorio();
+        num1.Mostrar();
+        num1.NumeroAleatorio1();
+        
+     
+        
+    }
+    
+}
